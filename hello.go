94d0017be5afcb3735f5ac0a0c4bfe8ece5dde06
Marius Kaurin <marius_kaurin@hotmail.no>
2017-01-25 15:49:08 +0100

    memes

--- hello.go
+++ hello.go
@@ -1,10 +1,7 @@
 package main
 
-import "os"
 import "fmt"
-import "strings"
 
 func main() {
-  name := os.Args[1:]
-  fmt.Printf("Hello, %s!\n", strings.Join(name, " "))
+  fmt.Println("Hello")
 }
