850c9d163b9ba6481dbda2e5a5e7c0453865f898
chinatsu <lolexplode@gmail.com>
2017-01-30 13:21:09 +0100

    Oppgave 1.2.4 4

---
+++ log/log/log.go
@@ -0,0 +1,7 @@
+package log
+
+import "math"
+
+func Log2(number float64) float64 {
+  return math.Log(number) / math.Log(2)
+}
---
+++ log/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+import "./log"
+
+func main() {
+  fmt.Println(log.Log2(16))
+}
